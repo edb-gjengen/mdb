5c847bcf3e6d68ddc2c2e2fd6ab974e94ca86430
Nikolai Kristiansen <nikolaik@gmail.com>
2012-01-13 01:22:25 +0100

    Adds a basic landing page.
    Uses the bootstrap css library from Twitter.

--- mdb/views.py
+++ mdb/views.py
@@ -1,1 +1,7 @@
-# Create your views here.
+from django.shortcuts import render_to_response
+from django.template import RequestContext
+
+def home(request):
+    return render_to_response('index.django.html', context_instance=RequestContext(request))
+
+
--- urls.py
+++ urls.py
@@ -1,17 +1,9 @@
 from django.conf.urls.defaults import patterns, include, url
-
-# Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'dns_mdb.views.home', name='home'),
-    # url(r'^dns_mdb/', include('dns_mdb.foo.urls')),
+    url(r'^$', 'mdb.views.home', name='home'),
 
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
     (r'^admin/', include(admin.site.urls)),
 )
