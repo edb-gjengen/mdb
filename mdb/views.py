f4b3d07bf47d6b865cab8f699ea8fe6d3bd31ad8
Tor Martin Slåen <tormsl@studentersamfundet.no>
2012-02-08 23:39:48 +0100

    started upgrading front-end

--- mdb/views.py
+++ mdb/views.py
@@ -4,4 +4,11 @@ from django.template import RequestContext
 def home(request):
     return render_to_response('index.django.html', context_instance=RequestContext(request))
 
+def index(request):
+	return render_to_response('info.django.html', context_instance=RequestContext(request))
 
+def host(request):
+	return render_to_response('host.django.html', context_instance=RequestContext(request))
+
+def host_detail(request, host_id):
+	return render_to_response('host.django.html', context_instance=RequestContext(request))
--- urls.py
+++ urls.py
@@ -1,9 +1,15 @@
 from django.conf.urls.defaults import patterns, include, url
 from django.contrib import admin
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+#from django.conf import settings
+
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    url(r'^$', 'mdb.views.home', name='home'),
-
-    (r'^admin/', include(admin.site.urls)),
+	url(r'^$', 'mdb.views.home', name='home'),
+	url(r'^info/', include('mdb.urls')),
+	url(r'^admin/', include(admin.site.urls)),
 )
+
+urlpatterns += staticfiles_urlpatterns()
+
